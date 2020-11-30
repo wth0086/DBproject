04978f4d265ca6f6410ec44d534cd78726c26beb
hyunsung71 <hongsunggg@naver.com>
2020-11-30 13:59:51 +0900

    before state

--- DBproject/employees/models.py
+++ DBproject/employees/models.py
@@ -22,15 +22,26 @@ class Employee(models.Model):
         (TYPE_RESERVATION, "Reservation"),
         (TYPE_ROOMMAID, "Room maid"),
         (TYPE_OTHER, "Other"),
-    ]
+        ]
     
-    ID = models.CharField(max_length=50, primary_key=True)
-    name = models.CharField(max_length=20, blank=True)
-    work_type = models.CharField(choices=TYPE_CHOICES, max_length=20, blank=True)
-    birthdate = models.DateField(blank=True, null=True)
-    address = models.CharField(max_length=200)
-    salary = models.IntegerField()
-    phone_number = models.CharField(max_length=100)
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+
+    GENDER_CHOICES = [
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+    ]
+
+
+    e_ID = models.CharField(max_length=20, primary_key=True)
+    e_PW = models.CharField(max_length=20)
+    e_name = models.CharField(max_length=20, default="-")
+    e_gender = models.CharField(choices=GENDER_CHOICES, max_length=20, blank=True)
+    e_work_type = models.CharField(choices=TYPE_CHOICES, max_length=20, blank=True)
+    e_birthdate = models.DateField(blank=True, null=True)
+    e_address = models.CharField(max_length=200, default="-")
+    e_salary = models.IntegerField(null=True, blank=True, default=0)
+    e_phone_number = models.CharField(max_length=100, default="000-0000-0000")
 
     def __str__(self):
-        return self.ID
+        return self.e_ID
