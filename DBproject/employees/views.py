4d779553381438048399a8b2179c651071f9725a
hyunsung71 <hongsunggg@naver.com>
2020-12-03 03:57:41 +0900

    add employee login

--- DBproject/employees/views.py
+++ DBproject/employees/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from employees.models import Employee
+from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import auth
 from django.contrib.auth import login
@@ -13,15 +14,26 @@ def login_page(request):
     return render(request, 'employees/login.html')
 
 def loginEmployee(request):
-    if request.method == "POST":
-        user = AuthenticationForm(request, request.POST)
-        if user.is_valid:
-            login(request, user.get_user())
-            return HttpResponseRedirect(reverse('employees:emAll'))
+    response_data = {}
+
+    if request.method == "GET":
+        return render(request, 'employees/login.html')
+
+    elif request.method == "POST":
+        login_ID = request.POST.get('ID', None)
+        login_PW = request.POST.get('PW', None)
+
+        if not(login_ID and login_PW):
+            response_data['error'] = "아이디와 비밀번호를 모두 입력해주세요"
         else:
-            return render(request, 'employees/login.html', {'error': 'ID or PW is incorrect'})
-    else:
-        render(request, 'employees/login.html')
+            myuser = Employee.objects.get(e_ID=login_ID)
+            if check_password(login_PW, myuser.e_PW):
+                request.session['user'] = myuser.e_ID
+                return HttpResponseRedirect(reverse('employees:emAll'))
+            else:
+                response_data['error'] = "비밀번호를 틀렸습니다."
+
+        return render(request, 'employees/login.html', response_data)
 
 
 def regEmployee(request):
@@ -29,22 +41,27 @@ def regEmployee(request):
 
 def regConEmployee(request):
     ID = request.POST['ID']
-    PW = request.POST['PW']
+    PW = request.POST['PW1']
+    re_PW = request.POST['PW2']
     name = request.POST['name']
     gender = request.POST['gender']
     work_type = request.POST['work_type']
     birthdate = request.POST['birthdate']
     address = request.POST['address']
     phone_number = request.POST['phone_number']
+    res_data = {}
 
-    qs = Employee(e_ID=ID, e_PW=PW, e_name=name, e_gender=gender, e_work_type =work_type, 
-    e_birthdate=birthdate, e_address=address, e_phone_number=phone_number)
-
-    if qs.is_valid():
+    if not (ID and PW and re_PW and name and gender and work_type and address and phone_number):
+            res_data['error'] = "모든 값을 입력해야 합니다."
+            return render(request, 'employees/registerEmployee.html', res_data)
+    if PW != re_PW:
+        res_data['error'] = '비밀번호가 다릅니다.'
+        return render(request, 'employees/registerEmployee.html', res_data)
+    else:
+        qs = Employee(e_ID=ID, e_PW=make_password(PW), e_name=name, e_gender=gender, e_work_type =work_type, 
+        e_birthdate=birthdate, e_address=address, e_phone_number=phone_number)
         qs.save()
         return HttpResponseRedirect(reverse('employees:login'))
-    else:
-        return render(request, 'employees/registerEmployee.html', {'error': 'There is a blank field'})
 
 def readEmployeeAll(request):
     qs = Employee.objects.all()
